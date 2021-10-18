22fdf0a27655136b288d80c40896b69c3fce08c1
gina khalisa <khalisa.gina18@gmail.com>
2021-10-18 12:26:53 +0700

    first commit

---
+++ challenge4.java
@@ -0,0 +1,16 @@
+public class challenge4 {
+    
+    static void SyaratTinggi(int tinggiBadan) {
+  
+      if (tinggiBadan >= 155) {
+        System.out.println("Anda Dapat Lanjut Test CPNS"); 
+        
+      } else {
+        System.out.println("Anda Tidak Dapat Lanjut Test CPNS"); 
+      }
+      
+    } 
+    public static void main(String[] args) { 
+      SyaratTinggi(145); 
+    }  
+  }                      
